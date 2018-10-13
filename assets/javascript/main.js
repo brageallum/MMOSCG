0086cd4f43aaaa59b75ddedde61380b21f864a52
Brage Allum <brageallum@gmail.com>
2018-10-13 22:54:03 +0200

    Snap scroll with css

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,5 @@
 var sections = document.getElementsByClassName('fullPageSection');
 var pageIndex = 0;
-var pageIsScrolling = false;
 
 window.scroll({
     top: 0,
@@ -12,49 +11,14 @@ document.addEventListener('keydown', (e) => {
 }, false);
 
 document.addEventListener('keyup', (e) => {
-    switch (e.keyCode) {
-        case 40:
-            pageIndex += 1;
-            goToSlide(pageIndex);
-        break;
-        case 38:
-            pageIndex -= 1;
-            if (goToSlide(pageIndex) == true) {
-                console.log('hi');
-            }
+    if (e.keyCode == 40 && pageIndex < sections.length - 1) {
+        goToSlide(++pageIndex);
+    } else if (e.keyCode == 38 && pageIndex > 0) {
+        goToSlide(--pageIndex);
     }
 }, false);
 
-function throttle(callback, timeDelta) {
-    var now = Date.now();
-
-    return (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        if (now + timeDelta <= Date.now()) {
-            callback(e);
-            now = Date.now();
-        }
-    };
-}
-
-
-document.addEventListener('wheel', throttle((e) => {
-        if (e.deltaY > 0) {
-            // DOWN
-            pageIndex = (pageIndex < (sections.length-1)) ? pageIndex + 1 : pageIndex;
-    
-            goToSlide(pageIndex);
-        } else if (e.deltaY < 0) {
-            // UP
-            pageIndex = (pageIndex > 0) ? pageIndex - 1 : pageIndex;
-            
-            goToSlide(pageIndex);
-        }
-    }, 1000));
-
 function goToSlide(i) {
-    console.log(i, sections[i].offsetTop);
     window.scroll({
         top: sections[i].offsetTop,
         behavior: "smooth"
