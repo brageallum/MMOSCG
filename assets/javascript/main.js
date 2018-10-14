3f7688cdd885c94003f15fb4cc3a773fe3a2e176
Brage Allum <brageallum@gmail.com>
2018-10-14 16:11:00 +0200

    Fixed navbar highlighting of the current page and modifyed how the navbar scrolls

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,14 +1,37 @@
-// Handels scrolling the page with the arrow keys and wasd keys
 var sections = document.getElementsByClassName('fullPageSection');
 
+function getCurrentPageIndex() {
+    var scrollY = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+    var pageIndex = Math.round(scrollY / window.innerHeight);
+    return pageIndex;
+}
+
+function goToSlide(i) {
+    window.scroll({
+        top: sections[i].offsetTop,
+        behavior: "smooth"
+    });
+}
+
+function goToSlideId(id) {
+    var section = document.querySelectorAll('.fullPageSection' + id)[0];
+    window.scroll({
+        top: section.offsetTop,
+        behavior: "smooth"
+    });
+}
+
+goToSlide(0);
+
+
+// Handles scrolling the page with the arrow keys and wasd keys
 document.addEventListener('keydown', (e) => {
     // Prevent default action for the keys used for scrolling
     [40, 38, 87, 83].includes(e.keyCode) ? e.preventDefault() : null;
 }, false);
 
 document.addEventListener('keyup', (e) => {
-    var scrollY = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
-    var pageIndex = Math.round(scrollY / window.innerHeight);
+    var pageIndex = getCurrentPageIndex();
 
     if ([40, 83].includes(e.keyCode) && pageIndex < sections.length - 1) {
         // SCROLL DOWN
@@ -19,30 +42,35 @@ document.addEventListener('keyup', (e) => {
     }
 }, false);
 
-function goToSlide(i) {
-    window.scroll({
-        top: sections[i].offsetTop,
-        behavior: "smooth"
+
+// Handle navigation with the navbar
+var navItems = document.querySelectorAll('#topBar .navItem');
+for (var i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        goToSlideId(e.target.getAttribute('href'));
     });
 }
-goToSlide(0);
 
-// Handles scrolling with navigation bar
-var navItem = document.getElementsByClassName('navItem');
 
-for(var i = 0; i < navItem.length; i++) {
-    (function(i) {
-        navItem[i].addEventListener('click', (e) => {
-            e.preventDefault();
-            for (var j = 0; j < navItem.length; j++) {
-                navItem[j].setAttribute('class', 'navItem');
-            }
-            navItem[i].setAttribute('class', 'navItem current');
-        }, false);
-    }(i));
-}
-
+// Handle parallax scrolling of the phone image in the hero
 var phone = document.getElementById('heroPhone');
 window.addEventListener('scroll', () => {
     phone.style.bottom = -200* (window.scrollY/window.innerHeight) +"px";
+});
+
+
+// Handle highlighting the correct link in the navbar
+function highlightCurrentPageInNavbar() {
+    var index = getCurrentPageIndex();
+    var sectionId = sections[index].getAttribute('id');
+    
+    if (document.querySelectorAll('#topBar .navItem[href="#' + sectionId + '"]').length == 0) return;
+        
+    document.querySelectorAll('#topBar .navItem.current')[0].classList.remove('current');
+    document.querySelectorAll('#topBar .navItem[href="#' + sectionId + '"]')[0].classList.add('current');
+}
+
+window.addEventListener('scroll', () => {
+    highlightCurrentPageInNavbar();
 });
