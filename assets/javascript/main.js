895f5288fd95a4dbc6cc3b00360ede2bc82323f7
Birk Ingebricson Wilberg <birk@ingebricson.no>
2018-10-14 04:58:17 +0200

    Made major changes to many different things

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,6 @@
 // Handels scrolling the page with the arrow keys and wasd keys
 var sections = document.getElementsByClassName('fullPageSection');
 
-window.scroll({
-    top: 0,
-    behavior: "instant"
-});
-
 document.addEventListener('keydown', (e) => {
     // Prevent default action for the keys used for scrolling
     [40, 38, 87, 83].includes(e.keyCode) ? e.preventDefault() : null;
@@ -30,3 +25,24 @@ function goToSlide(i) {
         behavior: "smooth"
     });
 }
+goToSlide(0);
+
+// Handles scrolling with navigation bar
+var navItem = document.getElementsByClassName('navItem');
+
+for(var i = 0; i < navItem.length; i++) {
+    (function(i) {
+        navItem[i].addEventListener('click', (e) => {
+            e.preventDefault();
+            for (var j = 0; j < navItem.length; j++) {
+                navItem[j].setAttribute('class', 'navItem');
+            }
+            navItem[i].setAttribute('class', 'navItem current');
+        }, false);
+    }(i));
+}
+
+var phone = document.getElementById('heroPhone');
+window.addEventListener('scroll', () => {
+    phone.style.bottom = -200* (window.scrollY/window.innerHeight) +"px";
+});
